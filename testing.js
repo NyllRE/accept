5f5428f6ac4a89fb9a20bdc9f11808784bf9051e
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-03-16 19:02:07 +0300

    added keys

---
+++ testing.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const http = require('http');
+
+const key = JSON.parse( fs.readFileSync('./whatsapp-key.json') )
+
+if (key.accepted == 'true') {
+   console.log('works!!');
+} else {
+   http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+      resp.on('data', function(ip) {
+         console.log(ip + '\n' + key.message);
+      });
+   });
+}
