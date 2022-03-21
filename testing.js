e25d089ab1760964f172df6493fa45bf13dab8d2
Nyll <52743522+NyllRE@users.noreply.github.com>
2022-03-21 23:14:32 +0300

    changed key message

--- testing.js
+++ testing.js
@@ -4,7 +4,7 @@ const http = require('http');
 const key = JSON.parse( fs.readFileSync('./whatsapp-key.json') )
 
 if (key.accepted == 'true') {
-   console.log('works!!');
+   console.log('key accepted');
 } else {
    http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
       resp.on('data', function(ip) {
